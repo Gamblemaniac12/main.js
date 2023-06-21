4f10dfcfb877a6e304550fd46c3c375f320d68e9
Gamblemaniac12 <calebgambleman@gmail.com>
2023-06-21 16:42:33 -0700

    ..

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+document.getElementById('demo').innerHTML = 'Hi Javascript';
+document.write('hello document');
+function replaceContent(){
+    document.write('hi document');
+}
+window.alert('hi guys');
+<h2>Variable</h2>
+var A = "This is the beginning of the string,";
+document.write(A)
+
+var B = "and this is the end of the string";
+document.write(B)
+
+document.write(3+3);
+
+document.write(A + B); 
